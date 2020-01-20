301d8b65297c6301a5fc231c34dbd6b88c6f4164
jgs9501 <jgs9501@naver.com>
2020-01-20 22:35:57 +0900

    2020/01/20 home.jsp -> index.jsp 갱신

--- src/main/java/com/junsoo/shopping/common/controller/HomeController.java
+++ src/main/java/com/junsoo/shopping/common/controller/HomeController.java
@@ -1,6 +1,17 @@
 package com.junsoo.shopping.common.controller;
 
-public interface HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-	public String home();
+/**
+ * 메인 홈 페이지 컨트롤러 작업
+ */
+@Controller
+public class HomeController{
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String main() {
+		return "index";
+	}
 }
