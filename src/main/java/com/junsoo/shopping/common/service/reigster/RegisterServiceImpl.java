0beda798cd93c01b9d37f3e869251606bc573ce4
jgs9501 <jgs9501@naver.com>
2020-10-24 18:17:06 +0900

    회원가입 ip설정 및 서버 글자깨짐 수정

--- src/main/java/com/junsoo/shopping/common/service/reigster/RegisterServiceImpl.java
+++ src/main/java/com/junsoo/shopping/common/service/reigster/RegisterServiceImpl.java
@@ -38,31 +38,31 @@ public class RegisterServiceImpl implements RegisterService {
 		try {
 			// 유저 ID 정규식 확인
 			if(!regexChecker.isUserId(vo.getUser_id())) {
-				error = "UserID input type error.";
+				error = "UserID input type error." + vo.getUser_id();
 			}
 			// 유저 패스워드 정규식 확인
 			else if(!regexChecker.isPassword(vo.getPassword())) {
-				error = "Password input type error.";
+				error = "Password input type error." + vo.getPassword();
 			}
 			// 유저 이름 정규식 확인
 			else if(!regexChecker.isUserName(vo.getUser_name())) {
-				error = "User name input type error.";
+				error = "User name input type error." + vo.getUser_name();
 			}
 			// 유저 이메일 정규식 확인 (UserId와 형식 동일하여 UserId사용)
 			else if(!regexChecker.isUserId(vo.getUser_email())) {
-				error = "User email input type error.";
+				error = "User email input type error." + vo.getUser_email();
 			}
 			// 유저 도메인 정규식 확인
 			else if(!regexChecker.isDomain(vo.getUser_domain())) {
-				error = "User domain input type error.";
+				error = "User domain input type error." + vo.getUser_domain();
 			}
 			// 유저 핸드폰 번호 정규식 확인
 			else if(!regexChecker.isPhone(vo.getUser_phone())) {
-				error = "User phone input type error.";
+				error = "User phone input type error." + vo.getUser_phone();
 			}
 			// 유저 우편번호 정규식 확인
 			else if(!regexChecker.isPost(vo.getUser_post())) {
-				error = "User post number input type error.";
+				error = "User post number input type error." + vo.getUser_post();
 			}
 			
 			// 유저 IP주소 기입
